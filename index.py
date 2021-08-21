984726c5198cf722e0beb2a3b3a9e32f7be769dc
Romel Niño Paano <rpaano02@gmail.com>
2021-08-22 01:20:00 +0800

    Change to one liner in some codes

--- index.py
+++ index.py
@@ -1,30 +1,21 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open("data.json", "r"))
+data = json.load(open("data.json"))
 
 
-def definition(word):
-    word = word.lower()
-    if word in data:
-        return data[word]
-    elif len(get_close_matches(word, data.keys())) > 0:
-        yn = input("Did you mean %s? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0]).upper()
-        if yn == "Y":
-            return data[get_close_matches(word, data.keys())[0]]
-        elif yn == "N":
-            return "Word does not exists."
-        else:
-            return "We did not understand your word."
+def definition(w):
+    w.lower()
+    if w in data:
+        return data[w]
+    elif len(close := get_close_matches(w, data.keys())) > 0:
+        yn = input(
+            "Did you mean %s? Please enter 'Y' for yes and 'N' for no: " % get_close_matches(w, data.keys())[0]).upper()
+        return data[close[0]] if yn == "Y" else (
+            "The word does not exists." if yn == "N" else "We did not understand your entry.")
     else:
-        return "The word does not exist."
+        return "The word doesn't exists. Please double check it."
 
 
-word = input("Enter word: ")
-
-defined = definition(word)
-
-if type(defined) == list:
-    print(*definition(word), sep="\n")
-else:
-    print(defined)
+print(*output if type(output := definition(input("Enter word: "))) == list else output,
+      sep="\n" if type(output) == list else "")
